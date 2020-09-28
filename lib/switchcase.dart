f2b40dfa74a6c470a4c19c890ff806d403855ab9
Kuuga <ryuki.servaiv@gmail.com>
2020-09-29 00:23:08 +0700

    variable, loop, conditions and statements

---
+++ lib/switchcase.dart
@@ -0,0 +1,13 @@
+void main(){
+  var grade = "";
+  switch(grade){
+    case "A": {print ("Very good");}
+    break;
+
+    case "B": {print ("Good");}
+    break;
+
+    default: {print ("No Choice");}
+    break;
+  }
+}
