ceaf1b13c0aa52698edd906f8b1a748d65da88b3
Kuuga <ryuki.servaiv@gmail.com>
2020-10-04 22:43:51 +0700

    class with param

--- lib/simple_class2.dart
+++ lib/simple_class2.dart
@@ -1,13 +1,22 @@
 void main() {
-  User userOne = new User();
-  print(userOne.age);
+  User userOne = new User('Luigi',30);
   print(userOne.username);
+  print(userOne.age);
+
+  User userTwo = User("Mario",40);
+  print(userTwo.username);
+  print(userTwo.age);
   userOne.login();
 }
 
 class User {
-  String username = 'mario';
-  int age = 25;
+  String username;
+  int age;
+
+  User(String username, int age) {
+    this.username = username;
+    this.age = age;
+  }
 
   void login() {
     print('user logged in');
